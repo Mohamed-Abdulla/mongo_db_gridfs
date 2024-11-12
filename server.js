25b00fcd37f222a41a2ee3a57130f533535b1432
Mohamed-Abdulla <abdabdulla78@gmail.com>
2024-11-12 16:34:38 +0530

    refactor: remove redundant CORS headers middleware

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ console.error = logger.error.bind(logger);
 // Apply CORS middleware
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(express.json());
 // app.use(apiKeyMiddleware);
